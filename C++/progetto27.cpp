c095d398316fa3040a8dc7a15154b117be965318
Alex Cornix <alex.cornacchini00@gmail.com>
2024-05-27 08:36:55 +0000

    prove in classe

---
+++ C++/progetto27.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"benvenuto su GIT POD"<<endl;
+    string name;
+    cout<<"scrivi il tuo nome: "<<endl;
+    cin>>name;
+    cout<<"ciao"<<name;
+    return 0;
+}
